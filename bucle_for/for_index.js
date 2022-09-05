72b838ca78f4d8b7f1599c765edbd12901b3f853
Oscar Maidana <odmaidana@yahoo.com.ar>
2022-09-05 19:26:36 -0300

    inicio  de repositorio

---
+++ bucle_for/for_index.js
@@ -0,0 +1,7 @@
+
+ let numero = parseInt(prompt("Ingrese el numero"));
+ for(let index = 0; index <= 10; index++){
+   let resultado = numero + index;
+   let mensaje = `${numero} + ${index} = ${resultado}`;
+   alert(mensaje);
+ }
